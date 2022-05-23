6adba5048dd85bcd494e82c7c7796fc93e0f7b70
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2022-05-23 17:33:20 +0200

    update einde les week 5

--- Week 5/Oef aggregatie/src/main/java/be/odisee/huiswerk4/Bank.java
+++ Week 5/Oef aggregatie/src/main/java/be/odisee/huiswerk4/Bank.java
@@ -5,22 +5,38 @@ import java.util.ArrayList;
 public class Bank {
     private ArrayList<Account> accounts = new ArrayList<>();
 
-    public void openAccount(Account account){
-        if(!accounts.contains(account)) {
-            accounts.add(account);
+    public double openAccount(boolean isSpaarrekening){
+        Account newAccount;
+        if(isSpaarrekening) {
+            newAccount = new SpaarRekening(0.0011);
+        } else {
+            newAccount = new ZichtRekening(200);
         }
+
+        accounts.add(newAccount);
+        return newAccount.getAccountNumber();
     }
 
-    public boolean accountExists(Account account){
-        return accounts.contains(account);
+    private Account findAccount(double id){
+        for (Account acc: accounts ) {
+            if(acc.getAccountNumber() == id){
+                return acc;
+            }
+        }
+        return null;
+    }
+
+    public boolean accountExists(double rekeningnummer){
+        return findAccount(rekeningnummer) != null;
     }
 
     public int numAccounts(){
         return accounts.size();
     }
 
-    public void closeAccount(Account account){
-        accounts.remove(account);
+    public void closeAccount(double rekeningnummer){
+        Account acc = findAccount(rekeningnummer);
+        accounts.remove(acc);
     }
 
     public void update(){
@@ -33,22 +49,18 @@ public class Bank {
         }
     }
 
+    public boolean deposit(double rekeningnummer, double amount){
+        return false;
+        //TODO: continue
+    }
+
     public static void main(String[] args) {
-        // Spaarrekeningen
-        SpaarRekening spRekening = new SpaarRekening(0.0011);
-        SpaarRekening spRekening2 = new SpaarRekening(1);
-        SpaarRekening spRekening3 = new SpaarRekening(0.10);
-
-        // ZichtRekening
-        ZichtRekening ziRekening = new ZichtRekening(10);
-        ZichtRekening ziRekening2 = new ZichtRekening(100);
-
         Bank bank = new Bank();
-        bank.openAccount(spRekening);
-        bank.openAccount(spRekening2);
-        bank.openAccount(spRekening3);
-        bank.openAccount(ziRekening);
-        bank.openAccount(ziRekening2);
+
+        // Spaarrekeningen
+        double sp1 = bank.openAccount(true);
+        double sp2 = bank.openAccount(true);
+        double sp3 = bank.openAccount(true);
 
         spRekening.deposit(100);
         spRekening2.deposit(0);
